4484a707e933aecb98d23e97d99e76defa6475f6
hiraro <traurig.orz@gmail.com>
2012-06-04 02:12:01 +0900

    init

---
+++ TSP/src/net/hirarirero/TSP/InputFileReader.java
@@ -0,0 +1,53 @@
+package net.hirarirero.TSP;
+
+import java.awt.Point;
+import java.awt.geom.Point2D;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.LineNumberReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class InputFileReader {
+
+	public static final String DELIM = " ";
+	private LineNumberReader lnr;
+
+	public InputFileReader(String pathname) {
+		this(new File(pathname));
+	}
+
+	public InputFileReader(File file) {
+		try {
+			lnr = new LineNumberReader(new FileReader(file));
+		} catch (FileNotFoundException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
+	}
+
+	public List<Point> readFile() {
+		String line;
+		ArrayList<Point> ret = new ArrayList<Point>();
+		StringTokenizer st;
+		int x, y;
+		try {
+			do {
+				line = lnr.readLine();
+				if (line != null) {
+					st = new StringTokenizer(line, DELIM);
+					x = Integer.parseInt(st.nextToken());
+					y = Integer.parseInt(st.nextToken());
+					ret.add(new Point(x, y));
+				}
+			} while (line != null);
+		} catch (Exception e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
+		return ret;
+	}
+}
---
+++ TSP/src/net/hirarirero/TSP/Main.java
@@ -0,0 +1,21 @@
+package net.hirarirero.TSP;
+
+import java.awt.Point;
+import java.util.HashSet;
+import java.util.List;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		InputFileReader ifr=new InputFileReader("data.txt");
+		List<Point> ls=ifr.readFile();
+		for(Point p:ls){
+			System.out.println(p);
+		}
+	}
+
+}
